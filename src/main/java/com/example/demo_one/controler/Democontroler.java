18b283647344cb77f5cb6d61573c54007a6fc8b2
ArtemCH240322 <sun2383@mail.ru>
2022-03-26 22:32:57 +0300

    hw 2.3 done

---
+++ src/main/java/com/example/demo_one/controler/Democontroler.java
@@ -0,0 +1,11 @@
+package com.example.demo_one.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class Democontroler {
+
+}
+@GetMapping apping
+public String print Hello() {
+    return "Hello world" ;}
+        }
